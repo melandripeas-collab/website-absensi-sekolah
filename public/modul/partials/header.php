15309850fedb3f12a0a9a7d6ad33c7c7dbf66aad
siscatiara2-dotcom <siscatiara2@gmail.com>
2026-08-10 14:20:38 +0700

    siska

--- public/modul/partials/header.php
+++ public/modul/partials/header.php
@@ -6,6 +6,7 @@
     <title>Website Absensi Sekolah</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body>
